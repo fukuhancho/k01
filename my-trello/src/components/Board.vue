984048b698ffad3a358dc8fc0b3216ae6f6f7db1
t-inuzuka <t-inuzuka@Op9020>
2022-08-01 23:28:05 +0900

    add dir

---
+++ my-trello/src/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <header>
+        my Trello
+    </header>
+    <main>
+        <p class="info-Line">All: 0 tasks</p>
+        <list-add />
+    </main>
+</div>
+</template>
+
+<script>
+import ListAdd from './ListAdd.vue'
+
+export default{
+
+    components: {
+        ListAdd
+    },
+
+}
+</script>
